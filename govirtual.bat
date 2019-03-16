887b5e3d392013667737b1abbb7e51219e9d8022
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2019-03-16 22:51:32 +0530

    Update govirtual.bat
    Added support to automatically install python libraries from requirement.txt file

--- govirtual.bat
+++ govirtual.bat
@@ -1,17 +1,26 @@
 @REM single command to activate python virtual environment
-@REM Author: Lakhya Jyoti Nath (ljnath) @ August 2018
+@REM supports automatic installation of libraries from requirement.txt
+@REM Author: Lakhya Jyoti Nath (ljnath) @ August 2018 | www.ljnath.com
 
 @echo off
 SET env_name=pyenv
+SET requirement_file=requirement.txt
+title GoVirtual 0.1 - %env_name%
+
 IF NOT EXIST %env_name% GOTO :CREATE_ENV
 
 :SWITCH_ENV
-	start %env_name%\Scripts\activate
-	GOTO :EXIT
+	call %env_name%\Scripts\activate.bat
+	IF EXIST %requirement_file% (
+		pip install -r %requirement_file%
+	)
+	GOTO :WELCOME
+	
 :CREATE_ENV
-	echo Virtual environment directory %env_name% is missing. Setting up virtual environment
-	virtualenv %env_name%
+	echo Python virtual environment directory %env_name% is missing. Creating it now...
+	python -m venv %env_name%
 	GOTO :SWITCH_ENV
-:EXIT
+	
+:WELCOME
+	start %env_name%\Scripts\activate.bat
 	exit
-
