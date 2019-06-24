2aef07a1c44df0a112ea0eb74911d4bfb7db615c
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2019-06-24 22:09:32 +0530

    Added govirtual for unix as well

---
+++ govirtual.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# single command to activate python virtual environment
+# supports automatic installation of libraries from requirement.txt
+# Author: Lakhya Jyoti Nath (ljnath) @ March 2019 | www.ljnath.com
+#
+# make sure to have python3 in the PATH variable
+
+ENV_NAME=pyenv
+REQUIREMENT_FILE=requirements.txt
+
+function create_environment()
+{
+	echo Python virtual environment $ENV_NAME is missing, creating it now...
+	python3 -m venv $ENV_NAME
+}
+
+function switch_environment()
+{
+	. $ENV_NAME/bin/activate
+}
+
+function install_dependencies()
+{
+	if [ -f "$REQUIREMENT_FILE" ]; then
+		pip install -r $REQUIREMENT_FILE
+	fi
+}
+
+if ! [ -d "$ENV_NAME/bin" ]; then
+	create_environment
+fi
+
+switch_environment
+install_dependencies
