ca27341d0ae068b14aabd9941436bb2d6eb33b86
ljnath <lakhya.sci@gmail.com>
2023-02-28 00:50:10 +0530

    0.3: updated govirtual command for windows

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ govirtual is a single comamnd to replace these
  - Now you can type 'govirtual' in any location.
    govirtual will check for deafult python virtual directory (pyenv), if existing directory is present it will activate it else it will create a new one and will activate it
  - If requirements.txt file is detected it will install the packages mentioned in it as well
+
+ 
+## Change Log
+### [0.3] - February 2023
+- optimized and refacting
+
+### [0.2] - May 2020
+- added support to pass custom python binary which will be used for virtual env creation.
+  useful for creating seperate x64 and x86 virtual environments
+
+### [0.1] - Aug 2018
+- initial release
--- govirtual.bat
+++ govirtual.bat
@@ -1,26 +1,37 @@
 @REM single command to activate python virtual environment
 @REM supports automatic installation of libraries from requirement.txt
-@REM Author: Lakhya Jyoti Nath (ljnath) @ August 2018 | www.ljnath.com
+@REM Author: Lakhya Jyoti Nath (ljnath) @ August 2018 - February 2023 | www.ljnath.com
+@REM
+@REM chnage-log
+@REM 0.2 - added support to pass custom python binary for creating virtual environment
+@REM 0.3 - optimzed and enhancement
 
 @echo off
-SET env_name=.venv
-SET requirement_file=requirements.txt
-title GoVirtual 0.1 - %env_name%
 
-IF NOT EXIST %env_name%\Scripts GOTO :CREATE_ENV
+REM Set the name of the virtual environment
+set env_name=.venv
 
-:SWITCH_ENV
-	call %env_name%\Scripts\activate.bat
-	IF EXIST %requirement_file% (
-		pip install -r %requirement_file%
-	)
-	GOTO :WELCOME
-	
-:CREATE_ENV
-	echo Python virtual environment directory %env_name% is missing. Creating it now...
-	python -m venv %env_name%
-	GOTO :SWITCH_ENV
-	
-:WELCOME
-	start %env_name%\Scripts\activate.bat
-	exit
+REM Check if virtual environment folder exists
+if exist %env_name% (
+    REM Activate the virtual environment if folder exists
+    call %env_name%\Scripts\activate.bat
+
+    REM Check if the switch to virtual environment was successful; if switch has failed, then the env is recreated
+    if "%VIRTUAL_ENV%" == "" (
+        echo Failed to switch to virtual environment. Recreating the environment...
+        rmdir /s /q %env_name%
+        python -m venv %env_name%
+        call %env_name%\Scripts\activate.bat
+    )
+) else (
+    REM Create the virtual environment
+    python -m venv %env_name%
+
+    REM Activate the virtual environment
+    call %env_name%\Scripts\activate.bat
+)
+
+REM Install required packages if requirements.txt file is found
+if exist requirements.txt (
+    pip install -r requirements.txt
+)
